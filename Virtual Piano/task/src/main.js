c0330be3946ea361721904b0428f8db21a32da21
mreimundo <71842379+mreimundo@users.noreply.github.com>
2021-12-28 15:39:23 -0300

    Files

---
+++ Virtual Piano/task/src/main.js
@@ -0,0 +1,55 @@
+let audio = document.createElement("AUDIO");
+
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyA") {
+        let A = new Audio('A.mp3');
+        audio.play();
+    }
+    if (event.code == "KeyS") {
+        let S = new Audio('S.mp3');
+        audio.play();
+    }
+    if (event.code == "KeyD") {
+        let D = new Audio('D.mp3');
+        audio.play();
+    }
+    if (event.code == "KeyF") {
+        let F = new Audio('F.mp3');
+        audio.play();
+    }
+    if (event.code == "KeyG") {
+        let G = new Audio('G.mp3');
+        audio.play();
+    }
+    if (event.code == "KeyH") {
+        let H = new Audio('H.mp3');
+        audio.play();
+    }
+    if (event.code == "KeyJ") {
+        let J = new Audio('J.mp3');
+        audio.play();
+    }
+    if (event.code == "KeyW") {
+        let W = new Audio('W.mp3');
+        audio.play();
+    }
+    if (event.code == "KeyE") {
+        let E = new Audio('E.mp3');
+        audio.play();
+    }
+
+    if (event.code == "KeyT") {
+        let T = new Audio('T.mp3');
+        audio.play();
+    }
+
+    if (event.code == "KeyY") {
+        let Y = new Audio('Y.mp3');
+        audio.play();
+    }
+
+    if (event.code == "KeyU") {
+        let U = new Audio('U.mp3');
+        audio.play();
+    }
+});
